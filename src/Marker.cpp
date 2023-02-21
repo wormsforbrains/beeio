baef14b7f29f7928ac396b195823553c9fe37f9d
wormsforbrains <k.klassen000@gmail.com>
2023-02-20 22:11:55 -0800

    Added operator << overload

--- src/Marker.cpp
+++ src/Marker.cpp
@@ -2,6 +2,12 @@
 
 namespace beeio {
 
+  std::basic_ofstream<uint8_t> &operator <<(std::basic_ofstream<uint8_t> &file, const Marker &marker) {
+    // write marker data to the file
+    file.write(marker.getMarkerData().data(), static_cast<std::streamsize>(marker.getMarkerData().size()));
+    return file;
+  }
+
   bool hasData = false;
   std::vector<uint8_t> segmentLength(2);
 
