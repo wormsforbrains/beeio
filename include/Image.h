8fcac1d20c9d840256e51152a99276eb43e45eb9
wormsforbrains <k.klassen000@gmail.com>
2023-02-20 21:36:23 -0800

    Moved ColorDepth and colorDepthMap

--- include/Image.h
+++ include/Image.h
@@ -9,19 +9,15 @@ namespace beeio {
 
   typedef std::variant<std::vector<uint8_t>, std::vector<uint32_t>> DataVector;
 
-  enum ColorDepth {
-    BINARY_BYTE, GRAY_BYTE, RGB_3BYTE, RGBA_4BYTE
-  };
-
-  std::map<beeio::ColorDepth, int> colorDepthMap = {
-      {beeio::ColorDepth::BINARY_BYTE, 1},
-      {beeio::ColorDepth::GRAY_BYTE,   8},
-      {beeio::ColorDepth::RGB_3BYTE,   24},
-      {beeio::ColorDepth::RGBA_4BYTE,  32}
-  };
 
   class Image {
     public:
+      enum ColorDepth {
+        BINARY_BYTE, GRAY_BYTE, RGB_3BYTE, RGBA_4BYTE
+      };
+
+      static const std::map<ColorDepth, int> colorDepthMap;
+
       // Factory methods
       static Image create(ColorDepth colorDepth, int width, int height);
       static Image create(ColorDepth colorDepth, int width, int height, uint32_t color);
