ae51a683834e7cc6d2ca197f48748127c2c014fd
wormsforbrains <k.klassen000@gmail.com>
2023-02-20 22:10:09 -0800

    Updated _write_header() method

--- src/jpeg.cpp
+++ src/jpeg.cpp
@@ -14,36 +14,18 @@ namespace beeio {
     _write_image_data(file);
   }
 
-  void JPEG::_write_header(std::ofstream &file) {
+  void JPEG::_write_header(std::basic_ofstream<uint8_t> &file) {
     // Write Start of Image marker
-    file << (unsigned char)0xff << (unsigned char)0xd8;
+    file << SOI();
 
     // Write application-specific segment
-    unsigned short segment_length = 16;
-    unsigned short version = 1;
-    unsigned char units = 1;
-    unsigned short x_density = 1;
-    unsigned short y_density = 1;
-    file << (unsigned char)0xff << (unsigned char)0xe0;
-    file.write(reinterpret_cast<char *>(&segment_length), sizeof(unsigned short));
-    file << "JFIF" << (unsigned char)0x00;
-    file.write(reinterpret_cast<char *>(&version), sizeof(unsigned short));
-    file.write(reinterpret_cast<char *>(&units), sizeof(unsigned char));
-    file.write(reinterpret_cast<char *>(&x_density), sizeof(unsigned short));
-    file.write(reinterpret_cast<char *>(&y_density), sizeof(unsigned short));
-    file << (unsigned char)0x00 << (unsigned char)0x00;
-    file << (unsigned char)0x00 << (unsigned char)0x00;
+    file << APP0();
 
+    // Write DQT marker
+    file << DQT(0, 0);
 
-    // Write Start of Frame marker
-    unsigned short length2 = 17;
-    unsigned char precision = 8;
-    file << (unsigned char)0xff << (unsigned char)0xc0;
-    file.write(reinterpret_cast<char *>(&length2), sizeof(unsigned short));
-    file.write(reinterpret_cast<char *>(&precision), sizeof(unsigned char));
-    file.write(reinterpret_cast<char *>(&height_), sizeof(int));
-    file.write(reinterpret_cast<char *>(&width_), sizeof(int));
-    file.write(reinterpret_cast<char *>(&num_components_), sizeof(unsigned char));
+    // Write SOF0 marker
+    file << SOF0(width_, height_, num_components_);
   }
 
   void JPEG::_write_image_data(std::basic_ofstream<uint8_t> &file) {
